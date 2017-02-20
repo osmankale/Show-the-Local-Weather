7f7ed3a0f7579bfc2137c2ceceeb6bb0611b6838
Osman Kale <osman.kale@protonmail.com>
2017-02-20 16:29:40 +0000

    some changes

--- app.js
+++ app.js
@@ -1,31 +1,14 @@
-var x = document.getElementById("demo");
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
+document.getElementById("geo").onclick = function() {myFunction()};
+
+function myFunction() {
+  var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+         myObj = JSON.parse(this.responseText);
+        document.getElementById("demo").innerHTML = myObj.lat + ' / ' + myObj.lon;
     }
-}
-function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude; 
+};
+xmlhttp.open("GET", "http://ip-api.com/json", true);
+xmlhttp.send();
 }
 
-function showError(error) {
-    switch(error.code) {
-        case error.PERMISSION_DENIED:
-            x.innerHTML = "User denied the request for Geolocation.";
-            break;
-        case error.POSITION_UNAVAILABLE:
-            x.innerHTML = "Location information is unavailable.";
-            break;
-        case error.TIMEOUT:
-            x.innerHTML = "The request to get user location timed out.";
-            break;
-        case error.UNKNOWN_ERROR:
-            x.innerHTML = "An unknown error occurred.";
-            break;
-    }
-}
-
-showError();
